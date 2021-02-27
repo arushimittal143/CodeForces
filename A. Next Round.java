5ae0c8c054567ff2ebb576206aaca8bb1491eec2
Arushi <arushi1881.cse18@chitkara.edu.in>
2021-02-27 17:19:03 +0530

    Create A. Next Round.java

---
+++ A. Next Round.java
@@ -0,0 +1,30 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+public class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int k=sc.nextInt();
+		int arr[]=new int[n];
+		int count=0;
+		for(int i=0;i<n;i++)
+		{
+			arr[i]=sc.nextInt();
+		}
+		for(int i=0;i<n;i++)
+		{
+		if(arr[k-1]<=arr[i] && arr[i]!=0)
+		{
+		  count++;	
+		}
+	    }
+		System.out.println(count);
+	}
+}
