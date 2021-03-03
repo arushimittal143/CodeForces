9cee847c9281246a1ab9b108eea8f7fbd1d1405e
Arushi <arushi1881.cse18@chitkara.edu.in>
2021-03-03 18:02:25 +0530

    Create A. Bear and Big Brother.java

---
+++ A. Bear and Big Brother.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+public class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int count=0;
+	    while(a<=b)
+	    {
+	    	a=3*a;
+	    	b=2*b;
+	    	count++;
+	    }
+	    System.out.println(count);
+	}
+}
