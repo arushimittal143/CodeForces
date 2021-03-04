750114d74d6413acdf45fea431ae16742cd3c49c
Arushi <arushi1881.cse18@chitkara.edu.in>
2021-03-04 22:13:41 +0530

    Create A. Elephant.java

---
+++ A. Elephant.java
@@ -0,0 +1,18 @@
+ 
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+public class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int x=sc.nextInt();
+		int count=x/5;
+		if(x%5!=0) count++;
+		System.out.println(count);
+	}
+}
