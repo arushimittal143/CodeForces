c9588877e6c37f468f104e5ccbffc73fc031a54e
Arushi <arushi1881.cse18@chitkara.edu.in>
2021-03-10 23:18:24 +0530

    Create A. Presents.java

---
+++ A. Presents.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+public class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes herezzz
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				if((a[j]-1)==i)
+				{
+				System.out.print(j+1+" ");
+				break;
+				}
+			}
+		}
+	}
+}
